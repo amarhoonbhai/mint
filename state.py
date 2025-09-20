d14146748f25ff929ef75d10f7de892347f5ac24
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-09-20 14:05:35 +0530

    Create state.py

---
+++ state.py
@@ -0,0 +1,11 @@
+# ad_bot/state.py
+listen_states = {}
+user_temp_clients = {}
+otp_inputs = {}
+user_accounts = {}
+user_ad_message = {}
+user_time_interval = {}
+premium_users = set()
+ad_tasks = {}
+user_groups = {}
+user_warnings = {}
