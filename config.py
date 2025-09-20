b1a2aa7454684c48202c7a95e0e49fc521b2f366
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-09-20 14:04:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ad_bot/config.py
+BOT_TOKEN = "your_bot_token"
+API_ID = 123456
+API_HASH = "your_api_hash"
+ADMIN_ID = 123456789
+FORCE_CHANNEL = "@YourChannel"
+FORCE_GROUP = "@YourGroup"
+BOT_USERNAME = "@YourBot"
+MIN_INTERVAL = 300  # in seconds
